53292f0c71f48198c8af520f8bbabcaad6d5232d
nahas <nahasnoushad04@gmail.com>
2023-02-09 08:58:54 +0530

    Day3

--- demo/src/app/cat/cat.component.ts
+++ demo/src/app/cat/cat.component.ts
@@ -12,7 +12,7 @@ export class CatComponent {
 "id":1,
 "name":"tom",
 "breed":"persian",
-"image":"https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
+"image":"githttps://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
 
     },
 
